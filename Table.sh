6aa9f157abcf6d83b4c2a8feaa1c03bc2edd5632
anikumarv <anilsun4u@gmail.com>
2023-07-18 23:16:53 -0400

    Completed - Table.sh -v2

--- Table.sh
+++ Table.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #table
-echo "Enter Table:"
+echo -e "Enter Table: \c"
 read Tab
+echo "-----------"
+a=0
+while [ $a -le 10 ]
+do
+	sum=`expr $Tab \* $a`
+	echo -e "$Tab * $a = $sum"
+	a=`expr $a + 1`
+done
+echo "-----------"
